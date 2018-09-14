a9ab7ba6395173128a44a366a199db7b8819c3f5
Tim <tim.fu00@gmail.com>
2018-09-14 19:31:19 +1000

    modified comments

--- characterList.py
+++ characterList.py
@@ -1,19 +1,19 @@
-# Writes to file 'characters.txt' a list of all printable characters from Unicode's BMP (ver 11.0)
-# Can be reconfigured to filter out any characters based on category, character set
+# Writes to file 'characters.txt' a list of all printable characters from Unicode's BMP (ver 11.0).
+# Can be reconfigured to filter out characters based on category, character set.
 
 import unicodedata
 import io
 
 
-# Checks if ith unicode character is in the range of specified character sets
-def setCheck(i, setList):
-    for s in setList:
-        if chrSet[s][0] <= i <= chrSet[s][1]:
+# Checks if unicode character is in specified character sets.
+def in_set(index, setFilter):
+    for s in setFilter:
+        if chrSet[s][0] <= index <= chrSet[s][1]:
             return True
     return False
 
 
-# Unicode Categories  ( more information: http://www.fileformat.info/info/unicode/category/index.htm )
+# Unicode Categories  ( from: http://www.fileformat.info/info/unicode/category/index.htm )
 control     = ('Cc', 'Cf', 'Cn', 'Co', 'Cs')
 separator   = ('Zl', 'Zp', 'Zs')
 letter      = ('LC', 'Ll', 'Lm', 'Lo', 'Lt', 'Lu')
@@ -23,7 +23,8 @@ punctuation = ('Pc', 'Pd', 'Pe', 'Pf', 'Pi', 'Po', 'Ps')
 symbol      = ('Sc', 'Sk', 'Sm', 'So')
 
 
-# Character sets and ranges ( from: https://en.wikipedia.org/wiki/List_of_Unicode_characters )
+# Character Sets and Ranges ( from: https://en.wikipedia.org/wiki/List_of_Unicode_characters )
+# *currently working to retrieve all character sets automatically.
 chrSet = {'basic latin': (32, 126),
           'Latin-1 Supplement': (160, 255),
           'Latin Extended-A': (256, 383),
@@ -31,17 +32,19 @@ chrSet = {'basic latin': (32, 126),
           'Latin Extended Additional': (647, 669)}
 
 
-# Filter lists
-catList = control + separator
-setList = ['basic latin', 'Latin Extended Additional']
+# Filters
+catFilter = control + separator
+setFilter = ['basic latin', 'Latin Extended Additional']
 
+
+# Gets unicode based on filters and writes them to file 'characters.txt'.
 f = io.open('characters.txt', 'w', encoding="utf-8")
 f.write('   index    code     character    category   name\n')
 
 c = 0
 for i in range(0, 65536):
-    if unicodedata.category(chr(i)) not in catList:
-        #if setCheck(i, setList):
+    if unicodedata.category(chr(i)) not in catFilter:
+        #if in_set(i, setFilter):
             glyph = chr(i)
             code = r"\u%04X" % i
             cat = unicodedata.category(glyph)
